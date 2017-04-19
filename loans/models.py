7d9fc57de133931757d2925e70adadc6c1176d10
angeosuinan <suinanangelo@gmail.com>
2017-04-19 12:57:09 +0800

    fix migration and db

--- loans/models.py
+++ loans/models.py
@@ -12,18 +12,21 @@ payment_methods=(
         ('SEMI-ANNUALLY', 'SEMI-ANNUALLY'),
         ('ANUALLY', 'ANNUALLY'),
         )
+users=()
+def get_Users():
+    #get all user and put it in options
+    pass
 class Mortgage(Base):
     due_date = models.DateTimeField(blank=True, null=True)
     balance = models.DecimalField(max_digits=1000, decimal_places=2, default=Decimal('0'))
-    loanee = models.OneToOneField(User, on_delete=models.CASCADE, default="",
-                    primary_key=True, blank=True)
+    loanee = models.CharField(max_length=300, default="")
     payment =models.CharField(max_length=100, choices=payment_methods,
                                 default='MONTHLY')
 class StudentLoan(Base):
     due_date = models.DateTimeField(blank=True, null= True)
     balance = models.DecimalField(max_digits=1000, decimal_places=2, default=Decimal('0'))
-    loanee = models.OneToOneField(User, on_delete = models.CASCADE, default ="",
-                    primary_key=True, blank=True)
+    loanee = models.CharField(max_length=300, default="")
+
     payment =models.CharField(max_length=100, choices=payment_methods,
                                 default='MONTHLY')
     def __str__(self):
@@ -31,8 +34,8 @@ class StudentLoan(Base):
 class AutoLoan(Base): 
     due_date = models.DateTimeField(blank=True, null =True)
     balance = models.DecimalField(max_digits=1000, decimal_places=2, default=Decimal('0'))
-    loanee = models.OneToOneField(User, on_delete=models.CASCADE, default='',
-                    primary_key=True, blank=True)
+    loanee = models.CharField(max_length=300,default="")
+
     payment =models.CharField(max_length=100, choices=payment_methods,
                                 default='MONTHLY')
     def __str__(self):
@@ -40,23 +43,19 @@ class AutoLoan(Base):
 class PersonalLoan(Base):
     due_date = models.DateTimeField(blank=True, null = True)
     balance = models.DecimalField(max_digits=1000, decimal_places=2, default=Decimal('0'))
-    loanee = models.OneToOneField(User, on_delete=models.CASCADE, default="",
-                    primary_key=True, blank=True)
     payment =models.CharField(max_length=100, choices=payment_methods,
                                 default='MONTHLY')
+    loanee = models.CharField(max_length=300, default="")
+
     def __str__(self):
         return (str(self.loanee))
 class HouseLoan(Base):
     due_date = models.DateTimeField(blank=True, null = True)
     balance = models.DecimalField(max_digits=1000, decimal_places=2, default=Decimal('0'))
-    loanee = models.OneToOneField(User, on_delete=models.CASCADE, default='',
-                    primary_key=True, blank=True, )
     payment =models.CharField(max_length=100, choices=payment_methods,
                                 default='MNT')
-    def __str__(self):
-        x=[]
-        x[0]=(str(self.loanee))
-        return x
+    loanee = models.CharField(max_length=300, default="")
+
 class Installments(Base):
     date_paid = models.DateTimeField(blank=True,null=True)
     price = models.DecimalField(max_digits=1000,decimal_places=2,default=Decimal('0'))
