1aa0e5dcf9d7675f35d7cca5a1f710a09e26897c
angeosuinan <suinanangelo@gmail.com>
2017-04-19 13:41:19 +0800

    finish GET Method javascript

--- loans/urls.py
+++ loans/urls.py
@@ -2,9 +2,10 @@
 from django.conf.urls import  include, url
 from django.contrib import admin
 
-from loans.api import views
-
+from loans.api import views2
+from . import views
 urlpatterns =[
-    url(r'^list/', views.MortgageList.as_view()),
+        url(r'^$',views.Index.as_view(),),
+    url(r'^list/', views2.MortgageList.as_view()),
    # url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),)
    ]
--- loans/views.py
+++ loans/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from django.http import HttpResponse
 # Create your views here.
+
+class Index(View):
+    template_name = 'loans/index.html'
+    def get(self,request):
+        if request.user.is_authenticated():
+            user = request.user
+            return render(request, self.template_name,{'user':user})
+        return HttpResponse("No User")
