99766567fe23d309dfb2f26c03adc0f7e6bd6645
angeosuinan <suinanangelo@gmail.com>
2017-04-22 22:03:50 +0800

    satisfied with apply loan

--- loans/api/serializers.py
+++ loans/api/serializers.py
@@ -1,10 +1,37 @@
-
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from loans.models import Mortgage
+from loans.models import Mortgage, PersonalLoan, HouseLoan, AutoLoan, StudentLoan
 
-class LoanSerializer(serializers.ModelSerializer):
+
+class MortgageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mortgage
+        fields = ['pk','loan_name','due_date','loanee',
+                'balance','payment','installment']
+
+class PersonalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PersonalLoan
+        fields = ['pk','loan_name','due_date','loanee',
+                'balance','payment','installment']
+
+
+class HouseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HouseLoan
+        fields = ['pk','loan_name','due_date','loanee',
+                'balance','payment','installment']
+
+
+class AutoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AutoLoan
+        fields = ['pk','loan_name','due_date','loanee',
+                'balance','payment','installment']
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudentLoan
         fields = ['pk','loan_name','due_date','loanee',
                 'balance','payment','installment']
--- loans/api/views2.py
+++ loans/api/views2.py
@@ -3,7 +3,8 @@ from rest_framework.parsers import JSONParser
 from django.contrib.auth.models import User
 from django.http import Http404, HttpResponse
 from django.utils.six import BytesIO
-from .serializers import LoanSerializer
+from .serializers import (MortgageSerializer, HouseSerializer, AutoSerializer,
+        PersonalSerializer, StudentSerializer)
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -13,6 +14,7 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from itertools import chain
 from collections import OrderedDict
+from datetime import date, timedelta
 class LoanList(APIView):
 
     def get(self, request, format=None):
@@ -24,16 +26,40 @@ class LoanList(APIView):
             A = AutoLoan.objects.filter(loanee=username)
             H = HouseLoan.objects.filter(loanee=username)
             result= list(chain(M, P, S, A, H))
-            serializer = LoanSerializer(result , many = True) #TO inherit
+            serializer = MortgageSerializer(result , many = True)
             content = JSONRenderer().render(serializer.data)
             return Response(serializer.data)
         return HttpResponse("LOGIN FIRST")
     
     @csrf_exempt
     def post(self,request,format=None):
-        serializer = LoanSerializer(data = request.data)
+        request.data['loanee']=request.user.username
+        d = ""
+        print request.data['payment']
+        if request.data['payment']=='MONTHLY':
+            d = date.today() + timedelta(days=30)
+        elif request.data['payment'] == 'SEMI-ANNUALLY':
+            d = date.today() + timedelta(days=185)
+        elif request.data['payment'] == 'ANNUALLY':
+            d = date.today() + timedelta(days=360)
+        request.data['due_date'] = d
+
+        serializer = MortgageSerializer(data = request.data)
+        print request.data['loan_name']
+        if request.data['loan_name'] == "MORTGAGE":
+            serializer = MortgageSerializer(data = request.data)
+        if request.data['loan_name'] == "PERSONAL":
+            print("SDDASDASDASC")
+            serializer = PersonalSerializer(data = request.data)
+        if request.data['loan_name'] == "AUTO":
+            serializer = AutoSerializer(data = request.data)
+        if request.data['loan_name'] == "HOUSE":
+            serializer = HouseSerializer(data = request.data)
+        if request.data['loan_name'] == "STUDENT":
+            serializer = StudentSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
+            print serializer.data
             return Response (serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
@@ -45,6 +71,7 @@ class InstallmentView(APIView):
     
     @csrf_exempt
     def post(self,request,format=None):
+        print ("DSADA")
         serializer = LoanSerializer(data = request.data)
         print len(request.data)
         if serializer.is_valid():
