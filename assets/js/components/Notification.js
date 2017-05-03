263da321e98a294070fe27b6f8b6209d139244ed
angeosuinan <suinanangelo@gmail.com>
2017-05-03 19:13:21 +0800

    added dumb component to notifications

--- assets/js/components/Notification.js
+++ assets/js/components/Notification.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import {bindActionCreators} from "redux";
 
 import { fetchNotifications, activeNotifications} from "../actions/notificationsActions";
+import LoanItem from "./LoanItem.js";
 
 @connect((store) => {
 
@@ -22,69 +23,54 @@ export default class Notification extends React.Component{
 
 	render(){
 		const {notifications} = this.props;
-    
+    var activeNotif;
     if(!notifications.length){
         return <p> No New Notifications</p>
+        {activeNotif}
        }
     const {dispatch} = this.props;
     const actions = bindActionCreators(activeNotifications, dispatch);
     const mappednotifications  = notifications.map(function(x){
             return (
-              <ul key = {x.pk}>
-                <div class="alert alert-dismissible alert-success">
-                  <button type="button" class="close" data-dismiss="alert">&times;</button>
-                  <strong>Congratulations!</strong> 
-                 <p onClick={() => actions(x.loan)} >Your Loan with an id number has {x.loan} been approved</p>
-                </div>
-              </ul>
-
+                  <NotificationDumb actions={actions} x={x}   />
               )
     
             });
     const { loanNotif } = this.props;
-    var activeNotif = function(){}
-    console.log(loanNotif);
+    
+    var x= loanNotif[0];
     if(loanNotif[0]){
-    activeNotif = function(){
-      return (
-        <div id="jumbo" class="panel panel-primary"> 
-                    <div class="panel-heading"> 
-                      <h1 class="panel-title">{loanNotif[0].loan_name}</h1> 
-                    </div> 
-                    <div class="spanel-body"> 
-                      <div class="list-group"> 
-                        <a href="#" class="list-group-item active"> 
-                          Nearest Due date: {loanNotif[0].due_date}
-                        </a> 
-                        <a href="#" class="list-group-item"> 
-                        Payment Method: {loanNotif[0].payment} 
-                        </a> 
-                        <a href="#" class="list-group-item active"> 
-                        Balance: {loanNotif[0].balance}
-                        </a> 
-                        <a href="#" class="list-group-item "> 
-                        Remaining Installment: {loanNotif[0].number_of_installments}
-                        </a> 
-                        <a href="#" class="list-group-item "> 
-                        Installments: installments
-                        </a> 
-                        <a href="#" class="list-group-item "> 
-                        Approve: {String(loanNotif[0].approve)}
-                        </a> 
-                      </div> 
-                    </div> 
-                  </div>
-
-        )
-    }
-  }
-  
+   
+      activeNotif= <ul ><LoanItem loan={x} /> </ul>;
+      
+      }
+      
 		return(
 			<div class="Notification">
 			{mappednotifications}
-      {activeNotif()}
+      {activeNotif}
 			</div>
 
 			)
 	}
+}
+
+class NotificationDumb extends React.Component {
+
+  render(){
+
+    const {actions, x} = this.props;
+    return (
+
+      <ul key = {x.pk}>
+        <div class="alert alert-dismissible alert-success">
+          <button type="button" class="close" data-dismiss="alert">&times;</button>
+          <strong>Congratulations!</strong> 
+         <p onClick={() => actions(x.loan)} >Your Loan with an id number has {x.loan} been approved</p>       
+        </div>
+
+      </ul>
+
+      )
+  }
 }
